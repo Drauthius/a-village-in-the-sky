9ef78546bcc1e2b5ba5f30974b6d3debf4f5d560
Drauthius <albert@diserholt.com>
2019-10-27 10:44:51 +0100

    Fill out the list of buildings with some information

--- src/game/gui/buildingitem.lua
+++ src/game/gui/buildingitem.lua
@@ -17,6 +17,8 @@ You should have received a copy of the GNU General Public License
 along with A Village in the Sky. If not, see <http://www.gnu.org/licenses/>.
 --]]
 
+local babel = require "lib.babel"
+
 local InfoPanelItem = require "src.game.gui.infopanelitem"
 
 local spriteSheet = require "src.game.spritesheet"
@@ -33,7 +35,49 @@ function BuildingItem:initialize(x, y, h, fontNormal, fontBold, entity)
 end
 
 function BuildingItem:drawOverride(offset)
-	spriteSheet:draw(self.sprite, self.x + offset + 2, self.y + 2)
+	local x, y = self.x + offset + 2, self.y + 2
+	local w, h = self.w - 4, self.h - 4
+	local th = self.fontBold:getHeight() * 1.2
+
+	if self.entity:has("ConstructionComponent") then
+		love.graphics.setFont(self.fontBold)
+		love.graphics.setColor(spriteSheet:getOutlineColor())
+		love.graphics.printf(babel.translate("Under construction"), x, y, w, "center")
+
+		love.graphics.setColor(1, 1, 1, 1)
+	end
+
+	y = y + th
+
+	spriteSheet:draw(self.sprite, x + (w - self.sprite:getWidth()) / 2, y)
+
+	love.graphics.setFont(self.fontNormal)
+	love.graphics.setColor(spriteSheet:getOutlineColor())
+
+	y = y + h - 2 * th
+	if self.entity:has("AssignmentComponent") then
+		local assignment = self.entity:get("AssignmentComponent")
+		love.graphics.printf(string.format("%s: %d/%d",
+		                                   babel.translate("Villagers"),
+		                                   assignment:getNumAssignees(),
+		                                   assignment:getMaxAssignees()),
+		                     x, y, w, "center")
+	end
+
+	y = y - th
+	if self.entity:has("RunestoneComponent") then
+		love.graphics.printf(string.format("%s: %d",
+		                                   babel.translate("Stage"),
+		                                   self.entity:get("RunestoneComponent"):getLevel()),
+		                     x, y, w, "center")
+	elseif self.entity:has("DwellingComponent") then
+		love.graphics.printf(string.format("%s: %.1f",
+		                                   babel.translate("Food"),
+		                                   self.entity:get("DwellingComponent"):getFood()),
+		                     x, y, w, "center")
+	end
+
+	love.graphics.setColor(1, 1, 1, 1)
 end
 
 function BuildingItem:select()
